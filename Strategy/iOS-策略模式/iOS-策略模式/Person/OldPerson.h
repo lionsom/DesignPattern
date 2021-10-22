0c13fc001ad9ffff76093c86c5670f47984c2777
lionsom <lionsom_lin@qq.com>
2021-10-22 15:22:48 +0800

    策略模式

---
+++ Strategy/iOS-策略模式/iOS-策略模式/Person/OldPerson.h
@@ -0,0 +1,15 @@
+//
+//  OldPerson.h
+//  iOS-策略模式
+//
+//  Created by eagle on 2019/7/9.
+//  Copyright © 2019 yongzhen. All rights reserved.
+//
+
+#import "Person.h"
+
+
+@interface OldPerson : Person
+
+@end
+
---
+++ Strategy/iOS-策略模式/iOS-策略模式/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  iOS-策略模式
+//
+//  Created by eagle on 2019/7/8.
+//  Copyright © 2019 yongzhen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Person.h"
+#import "ChildPerson.h"
+#import "AdultPerson.h"
+#import "OldPerson.h"
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        Person *child = [[ChildPerson alloc] init];
+        [child eatSomeThing];
+        [child runEveryDay];
+        
+        NSLog(@"--------------");
+        
+        Person *adult = [[AdultPerson alloc] init];
+        [adult eatSomeThing];
+        [adult runEveryDay];
+        
+        NSLog(@"--------------");
+        
+        Person *old = [[OldPerson alloc] init];
+        [old eatSomeThing];
+        [old runEveryDay];
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
